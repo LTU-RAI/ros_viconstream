7d1d67383ac9ca0f096428d93463e6a4e7cceed7
Emil Fresk <emil.fresk@gmail.com>
2016-01-11 10:43:18 +0100

    Started implementation

--- src/ros_viconstream_node.cpp
+++ src/ros_viconstream_node.cpp
@@ -1,8 +1,52 @@
+#include <iostream>
+
 #include "ros/ros.h"
 #include "viconstream/viconstream.h"
+#include "geometry_msgs/PoseStamped.h"
+
+using namespace std;
+
+void vicon_cb(const Client &frame)
+{
+    cout << "Frame: " << frame.GetFrameNumber().FrameNumber << endl;
+}
 
 int main(int argc, char *argv[])
 {
+    /*
+     * Initializing ROS
+     */
+    ROS_INFO("Initializing Vicon...");
+    ros::init(argc, argv, "ros_viconstream");
+    ros::NodeHandle n("~");
+
+    /* Get the Vicon URL. */
+    std::string vicon_url;
+
+    if (!n.getParam("vicon_url", vicon_url))
+    {
+        ROS_WARN("No Vicon URL found defaulting to localhost.");
+        vicon_url = "localhost:801";
+    }
+
+    /* Connect to vicon. */
+    ViconStream::ViconStream vs(vicon_url, cout);
+
+    /* Subscribe to the vicon frames. */
+    vs.registerCallback(vicon_cb);
+
+    /* Enable the stream and check for errors. */
+    if (!vs.enableStream(true, false, false, false, StreamMode::ServerPush))
+    {
+        ROS_ERROR("Unable to connect to vicon!");
+
+        return 0;
+    }
+
+    ros::spin();
+
+    /* Disable the stream before closing. */
+    vs.disableStream();
 
     return 0;
 }
