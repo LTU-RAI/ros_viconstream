3729b3a87961004a9a1734eb2fbcf1826008344f
Emil Fresk <emil.fresk@gmail.com>
2016-01-14 15:59:36 +0100

    Added license and commenting.

--- src/ros_viconstream.h
+++ src/ros_viconstream.h
@@ -1,3 +1,23 @@
+/****************************************************************************
+*
+* Copyright (C) 2016 Emil Fresk.
+* All rights reserved.
+*
+* This file is part of the ROS ViconStream node.
+*
+* GNU Lesser General Public License Usage
+* This file may be used under the terms of the GNU Lesser
+* General Public License version 3.0 as published by the Free Software
+* Foundation and appearing in the file LICENSE included in the
+* packaging of this file.  Please review the following information to
+* ensure the GNU Lesser General Public License version 3.0 requirements
+* will be met: http://www.gnu.org/licenses/lgpl-3.0.html.
+*
+* If you have questions regarding the use of this file, please contact
+* Emil Fresk at emil.fresk@gmail.com.
+*
+****************************************************************************/
+
 #include <iostream>
 #include <map>
 #include <vector>
@@ -8,8 +28,6 @@
 #include "tf/tf.h"
 #include "tf/transform_broadcaster.h"
 #include "geometry_msgs/PoseStamped.h"
-
-
 
 
 class ROS_ViconStream
@@ -37,12 +55,34 @@ private:
     /* @brief ViconStream object. */
     ViconStream::ViconStream *_vs;
 
+    /**
+     * @brief   Registers or finds the corresponding Vicon object.
+     *
+     * @param[in] subjectName   The subject name from the Vicon frame.
+     * @param[in] segmentName   The segment name from the Vicon frame.
+     */
     ROS_ViconStream::ObjectPublisher&registerObject(std::string &subjectName,
                                                     std::string &segmentName);
+
+    /**
+     * @brief   Callbakc for the ViconStream.
+     *
+     * @param[in] frame Read only frame from the ViconStream.
+     */
     void viconCallback(const Client &frame);
 
 public:
+
+    /**
+     * @brief   Constructor for the ROS_ViconStream.
+     *
+     * @param[in] os Reference to the log output stream.
+     */
     ROS_ViconStream(std::ostream &os);
+
+    /**
+     * @brief   Destructor that handles the graceful exit of the Vicon.
+     */
     ~ROS_ViconStream();
 
 };
