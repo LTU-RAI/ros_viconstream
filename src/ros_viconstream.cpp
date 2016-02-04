02fdac6853d202d0605f4eb3db1a1b91016f2777
Emil Fresk <emil.fresk@gmail.com>
2016-02-04 09:37:58 +0100

    Fixed error in the cal multiplication

--- src/ros_viconstream.cpp
+++ src/ros_viconstream.cpp
@@ -269,7 +269,7 @@ void ROS_ViconStream::viconCallback(const Client &frame)
                                            rotation.Rotation[3]) ); // w
 
             /* Apply calibration. */
-            tf *= obj.zero_pose;
+            tf = tf * obj.zero_pose;
 
             /* Save all transform for the TransformBroadcaster. */
             tf_list.push_back( tf::StampedTransform( tf,
